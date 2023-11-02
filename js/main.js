7c6f631bcd9fb9ec1130c9c3409b112bd0176920
Abdulqodir <abdulqodirmaxmadaliyev03@gmail.com>
2023-11-02 09:30:27 +0500

    accordion, tab

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 $(document).ready(function () {
-    $(".popup-youtube").magnificPopup({
+    $(".youtube, .popup-vimeo, .popup-gmaps").magnificPopup({
+        disableOn: 700,
         type: "iframe",
+        mainClass: "mfp-fade",
+        removalDelay: 160,
+        preloader: false,
+        fixedContentPos: false,
     });
 });
 
