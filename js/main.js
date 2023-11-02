ef25d55088c2220ce878b05ea32e4017ec6fe40e
Abdulqodir <abdulqodirmaxmadaliyev03@gmail.com>
2023-11-02 16:01:35 +0500

    function

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
-$(".youtube").magnificPopup({
-    disableOn: 700,
-    type: "iframe",
-    // mainClass: "mfp-fade",
-    removalDelay: 160,
-    preloader: false,
-    fixedContentPos: false,
+$(document).ready(function () {
+    $(".youtube, .popup-vimeo, .popup-gmaps").magnificPopup({
+        disableOn: 700,
+        type: "iframe",
+        mainClass: "mfp-fade",
+        removalDelay: 160,
+        preloader: false,
+        fixedContentPos: false,
+    });
 });
 
 AOS.init({
